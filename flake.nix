b813297d6595066f18f1c44cb7dd3ccc41004e2e
indium114 <156162907+indium114@users.noreply.github.com>
2026-07-22 18:06:11 +0200

    chore: use naersk for nix build

--- flake.nix
+++ flake.nix
@@ -4,13 +4,27 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      naersk,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
-      in {
+
+        naersk' = pkgs.callPackage naersk { };
+      in
+      {
         devShells.default = pkgs.mkShell {
           name = "rust-devshell";
 
@@ -24,18 +38,14 @@
           ];
         };
 
-        packages.usefulog = pkgs.rustPlatform.buildRustPackage {
-          name = "usefulog";
-          version = "0.1.1";
-
+        packages.usefulog = naersk'.buildPackage {
           src = ./.;
-
-          cargoLock.lockFile = ./Cargo.lock;
         };
 
         apps.usefulog = {
           type = "app";
           program = "${self.packages.${pkgs.stdenv.hostPlatform.system}.usefulog}/bin/usefulog";
         };
-      });
+      }
+    );
 }
